0a71a7d4cf001d46498b1a36802a8103947e168f
RISHII7 <rishikesh.palande07@gmail.com>
2024-11-11 18:39:33 +0530

    Setup React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import Layout from "@/components/layout";
 import { ThemeProvider } from "@/context/theme-provider";
@@ -6,22 +8,23 @@ import { ThemeProvider } from "@/context/theme-provider";
 import CityPage from "@/pages/city-page";
 import WeatherDashboard from "@/pages/weather-dashboard";
 
+const queryClient = new QueryClient();
 
 function App() {
-
   return (
-    <div>
-      <BrowserRouter>
-        <ThemeProvider defaultTheme="dark">
-          <Layout>
-            <Routes>
-              <Route path="/" element={<WeatherDashboard />} />
-              <Route path="/city/:cityName" element={<CityPage />} />
-            </Routes>
-          </Layout>
-        </ThemeProvider>
-      </BrowserRouter>
-    </div>
+    <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <ThemeProvider defaultTheme="dark">
+            <Layout>
+              <Routes>
+                <Route path="/" element={<WeatherDashboard />} />
+                <Route path="/city/:cityName" element={<CityPage />} />
+              </Routes>
+            </Layout>
+          </ThemeProvider>
+        </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
   )
 }
 
