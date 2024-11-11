da81a969eda1c7549bf9345a6ec400ffdf08e889
RISHII7 <rishikesh.palande07@gmail.com>
2024-11-11 18:15:55 +0530

    App Routes and Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import { Button } from "./components/ui/button"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+import Layout from "@/components/layout";
+import { ThemeProvider } from "@/context/theme-provider";
+
+import CityPage from "@/pages/city-page";
+import WeatherDashboard from "@/pages/weather-dashboard";
+
 
 function App() {
 
   return (
     <div>
-      <Button onClick={}>
-        Hello
-      </Button>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="dark">
+          <Layout>
+            <Routes>
+              <Route path="/" element={<WeatherDashboard />} />
+              <Route path="/city/:cityName" element={<CityPage />} />
+            </Routes>
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   )
 }
